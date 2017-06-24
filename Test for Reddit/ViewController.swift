8987b35902182c7988aac39a3eefa8a508f79876
Paul Dewaal <viper@viper-mm.lan>
2017-06-24 16:59:41 -0700

    Initial Commit

---
+++ Test for Reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test for Reddit
+//
+//  Created by Paul Dewaal on 6/24/17.
+//  Copyright © 2017 Paul Dewaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
