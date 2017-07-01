020ff3f959e016ac9f3c7cfe3446d7debb922451
Paul Dewaal <viper@viper-mm.lan>
2017-07-01 12:50:47 -0700

    Almost forgot adding support to save images to the album.

--- Test for Reddit/FullScreenImageVC.swift
+++ Test for Reddit/FullScreenImageVC.swift
@@ -12,18 +12,46 @@ class FullScreenImageVC : UIViewController {
     
     var link : RedditLink?
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var btnSave: UIButton!
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        btnSave.isUserInteractionEnabled = false
         link?.fullsize(completion: { (image) in
+            self.btnSave.isUserInteractionEnabled = true
             self.imgView.image = image
         })
     }
     
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction func savedTapped(_ sender: Any) {
+        btnSave.isUserInteractionEnabled = false
+        if let image = imgView.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    
+    @IBAction func closeTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        btnSave.isUserInteractionEnabled = true
+        if let error = error as NSError? {
+            var message = error.localizedDescription
+            if error.code == -3310 {
+                message = "Please turn on photos access for this app in the settings menu."
+            }
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+        else {
+            let alert = UIAlertController(title: "Saved", message: "The image was saved to your album.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
     }
 }
